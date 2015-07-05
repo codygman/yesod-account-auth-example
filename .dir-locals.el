9d5ddccc9cf09fbe9619211faa35a62d5b46f933
Cody Goodman <codygman.consulting@gmail.com>
2015-07-05 04:34:59 -0500

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((haskell-mode . ((haskell-indent-spaces . 4)
+                  (haskell-process-use-ghci . t)
+                  (hindent-style . "johan-tibell")
+                  (haskell-process-type . ghci)
+                  (haskell-process-path-ghci . "stack")
+                  (haskell-process-args-ghci . ("ghci"))))
+ (hamlet-mode . ((hamlet/basic-offset . 4)
+                 (haskell-process-use-ghci . t))))
+
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "idk" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
